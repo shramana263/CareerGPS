c61370487ee805fa24db3ff52bcd0203215fd67c
shramana263 <shramana652@gmail.com>
2025-04-10 03:26:43 +0530

    Add database configuration and user model implementation
    - Implement database connection using SQLAlchemy with environment variable support.
    - Create User model with relationships to skills and applications.
    - Define user_skill association table for many-to-many relationship.

---
+++ app/core/settings.py
@@ -0,0 +1,2 @@
+
+# DATABASE_URL=postgresql://jobreccdb_owner:npg_WdFQAET98yhU@ep-damp-tree-a18ipee2-pooler.ap-southeast-1.aws.neon.tech/jobreccdb?sslmode=require
---
+++ app/models/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Boolean , Date, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+
+user_skill= Table(
+    "user_skill",
+    Base.metadata,
+    Column("user_id",Integer, ForeignKey("user.id"), primary_key=True),
+    Column("skill_id", Integer, ForeignKey("skill.id"), primary_key=True)
+)
+
+class User(Base):
+    __tablename__= "users"
+    
+    id= Column(Integer, primary_key= True, index= True)
+    email= Column(String, unique= True, index= True)
+    hashed_password = Column(String)
+    full_name= Column(String)
+    is_active= Column(Boolean, default=True)
+    
+    #Resume details 
+    experience_years= Column(Integer, default=0)
+    education= Column(String, default="")
+    
+    #relationships
+    skills= relationship("Skill", secondary= user_skill, back_populates="users")
+    applications= relationship("Application", back_populates="user")

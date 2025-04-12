e2c8ba7f251274e0b35895b9df5352cdfea0cd3a
shramana263 <shramana652@gmail.com>
2025-04-12 23:54:56 +0530

    Remove unused import of MetaData from base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.schema import MetaData
 from app.core.config import settings
 
 engine = create_engine(str(settings.DATABASE_URL))
